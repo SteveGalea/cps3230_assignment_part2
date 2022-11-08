696186b5805b3b7b3aa1401eff20dee0327c7f83
Steve Galea <steve.galea.20@um.edu.mt>
2022-11-09 00:35:40 +0100

    initial setup

---
+++ src/test/java/test/website/TestRunner.java
@@ -0,0 +1,12 @@
+package test.website;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resource/features"
+)
+public class TestRunner {
+}
